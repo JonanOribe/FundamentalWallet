8a1faa62704ed8c255cb2e2693ff5efc51b4bcd5
Jon Ander Oribe <jonan.oribe@gmail.com>
2022-08-06 17:09:23 +0200

    Update TableInputComponent.vue

--- src/components/TableInputComponent.vue
+++ src/components/TableInputComponent.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="container input-area" style="margin-top: 0.5%;">
-    <p class="animate-charcter">Add your new stock >>>    <button class="btn btn-outline-success settings" @click="showNewStockModal()"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+    <p class="animate-charcter">Add your new stock >>>   
+  
+  <button class="btn btn-outline-success settings" @click="showNewStockModal()"  v-if="this.showAddNewStock===false"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
   <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
   <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
 </svg>
-</button></p>
+</button>
+
+  <button class="btn btn-outline-success settings" @click="showNewStockModal()"  v-if="this.showAddNewStock===true"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-dash-circle" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>
+</svg>
+</button>
+</p>
 <div v-show="showAddNewStock">
-    <input class="input-elements" v-model="stockName" placeholder="Add new stock symbol" />
+    <input class="input-elements" v-model="stockName" placeholder="APPL" />
     <button class="btn btn-outline-success settings" style="margin-left: 0.5%;" @click="updateStockList(),showNewStockModal()">Add Stock to list</button>
     <div  style="margin-top:0.5%">Select acquisition condition:
 
@@ -38,7 +47,7 @@ export default {
       client_id: localStorage.getItem("client_id"),
       statusPicked: "long",
       stockQuantity:1,
-      stockPrice:10.5
+      stockPrice:100.5
     };
   },
   methods: {
@@ -108,6 +117,7 @@ h3 {
 .input-elements{
     border: 3px solid #198754;
     border-radius: 25px;
+    text-align: center;
 }
 
 .animate-charcter
